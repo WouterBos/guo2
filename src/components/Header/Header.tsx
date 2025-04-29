cec8c373e5a74db0cb659d0707fd45326bce9266
Wouter Bos <wouter@bososs.nl>
2025-04-30 00:21:03 +0200

    Styleling and more photos

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,34 @@
 "use client";
 
-import logo from '../../assets/logo-guo-text-optimised.svg';
-import Title  from "./Title/Title";
+import logo from "../../assets/logo-guo-text-optimised.svg";
+import Title from "./Title/Title";
 import TextBlock from "./TextBlock/TextBlock";
 
 const Header: React.FC = () => {
-  return <Title>
-    <img src={logo} alt="Logo Groeten uit Oss" title="Groeten uit Oss" style={{ display: 'inline-block', width: 'auto', height: '4rem' }} />
-    <TextBlock>
-      Photography in the municipality<br />
-      of Oss, The Netherlands.
-    </TextBlock>
-    <TextBlock>
-      Available on <a href="https://www.facebook.com/GroetenUitOss">Facebook</a>,<br />
-      <a href="https://www.instagram.com/groetenuitoss/">Instagram</a> and <a href="https://mastodon.nl/@wouterbos">Mastodon</a>.
-    </TextBlock>
-  </Title>;
+  return (
+    <Title>
+      <img
+        src={logo}
+        alt="Logo Groeten uit Oss"
+        title="Groeten uit Oss"
+        style={{ display: "inline-block", width: "auto", height: "4rem" }}
+      />
+      <TextBlock>
+        <span>
+          Photography in the municipality
+          <br />
+          of Oss, The Netherlands.
+        </span>
+      </TextBlock>
+      <TextBlock>
+        <span>
+          Available on <a href="https://www.facebook.com/GroetenUitOss">Facebook</a>,<br />
+          <a href="https://www.instagram.com/groetenuitoss/">Instagram</a> and{" "}
+          <a href="https://mastodon.nl/@wouterbos">Mastodon</a>.
+        </span>
+      </TextBlock>
+    </Title>
+  );
 };
 
 export default Header;
