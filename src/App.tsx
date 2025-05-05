baa3ba72ed77715290573f443e111ef24c0d030d
Wouter Bos <wouter@bososs.nl>
2025-05-05 21:29:49 +0200

    Animation in image list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Header from './components/Header/Header';
-import ImageList from './components/Gallery/Gallery';
+import Gallery from './components/Gallery/Gallery';
 import images from "./images.json";
 
 function App() {
   return (
     <>
       <Header />
-      <ImageList data={images} />
+      <Gallery data={images} />
     </>
   )
 }
