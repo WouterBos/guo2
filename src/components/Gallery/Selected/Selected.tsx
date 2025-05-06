bf9b48e01f6e85b8c06309cf6bb3da3fbe44c452
Wouter Bos <wouter@bososs.nl>
2025-05-06 23:24:31 +0200

    Style tweak

--- src/components/Gallery/Selected/Selected.tsx
+++ src/components/Gallery/Selected/Selected.tsx
@@ -1,21 +1,33 @@
 "use client";
 
 import css from "./Selected.module.css";
+import { AnimatePresence, motion } from "framer-motion";
 
-export const Selected: React.FC<{ code: string; description: string }> = ({
-  code,
-  description,
-}) => {
+export const Selected: React.FC<{ code: string; description: string }> = ({ code, description }) => {
+  const variants = {
+    visible: { opacity: 1, transition: { duration: .3 } },
+    hidden: { opacity: 0, transition: { duration: .1 } }
+  }
+
   return (
     <div className={css.selected}>
-      <img src={`/groetenuitoss/photos/${code}.avif`} alt={description} />
-      <div>
-        <span>{description}</span>
-        <div className={css.selected}>{code}</div>
-      </div>
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={code}
+          initial={variants.hidden}
+          animate={variants.visible}
+          exit={variants.hidden}
+          style={{ width: 'fit-content', display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+        >
+          <img src={`/groetenuitoss/photos/${code}.avif`} alt={description} />
+          <div className={css.description}>
+            <span>{description}</span>
+            <div>{code}</div>
+          </div>
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 };
-
 
 export default Selected;
