0dfb06eb2b5fd07eab0c8c4de7bee0a04de999b6
Wouter Bos <wouter@bososs.nl>
2026-03-05 21:42:39 +0100

    Improve deploy-photos.cjs

--- deploy-photos.cjs
+++ deploy-photos.cjs
@@ -2,16 +2,25 @@ const FtpDeploy = require("ftp-deploy");
 const credentials = require("./deploy-credentials");
 const ftp = new FtpDeploy();
 
+console.log("🚀 Uploading photos of the last two months...");
+
 ftp.on("uploading", function (data) {
   console.log(`📦 ${data.filename}`);
 });
 
-const currentYear = new Date().getFullYear();
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const currentMonth = String(currentDate.getMonth() + 1).padStart(2, '0');
+
+const previousDate = new Date(currentDate);
+previousDate.setMonth(previousDate.getMonth() - 1);
+const previousYear = previousDate.getFullYear();
+const previousMonth = String(previousDate.getMonth() + 1).padStart(2, '0');
 
 const config = {
   ...credentials,
   deleteRemote: false,
-  include: [`${currentYear}*`],
+  include: [`${currentYear}${currentMonth}*`, `${previousYear}${previousMonth}*`],
   localRoot: __dirname + "/dist/photos",
   remoteRoot: "/groetenuitoss/photos",
 };
